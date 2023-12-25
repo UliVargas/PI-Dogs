a7ae70b8d1ff40288f2dc2b593d25b159a321e97
Ulises Vargas <uli.vargas02@gmail.com>
2023-12-25 04:29:53 -0600

    Agrego configuración básica de coverage

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '**/*.{js,ts}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_module']
 }
